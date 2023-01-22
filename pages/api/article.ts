099f0863cc476d7d53c969f60f2a3e80692985cc
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-01-23 02:29:09 +1100

    Add API endpoints for new User, Article models
    API endpoints to perform CRUD on new models

---
+++ pages/api/article.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { articleService } from '@lib/mongoose/services';
+import dbConnect from '@lib/mongoose/db';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+
+  switch (req.method) {
+    case 'GET':
+      try {
+        const article = await articleService.getArticle(req.query.id as string);
+        if (!article) {
+          res.status(404).json({ message: 'article not found' });
+        } else {
+          res.status(200).json(article);
+        }
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    case 'POST':
+      try {
+        const userId = req.user._id;
+
+        const article = await articleService.createArticle(req.body, userId);
+        res.status(201).json(article);
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    case 'PUT':
+      try {
+        const article = await articleService.updateArticle(
+          req.query.id as string,
+          req.body
+        );
+        if (!article) {
+          res.status(404).json({ message: 'article not found' });
+        } else {
+          res.status(200).json(article);
+        }
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    case 'DELETE':
+      try {
+        const article = await articleService.deleteArticle(
+          req.query.id as string
+        );
+        if (!article) {
+          res.status(404).json({ message: 'article not found' });
+        } else {
+          res.status(200).json({ message: 'article deleted successfully' });
+        }
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
---
+++ pages/api/user.ts
@@ -0,0 +1,63 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { userService } from '@lib/mongoose/services';
+import dbConnect from '@lib/mongoose/db';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+
+  switch (req.method) {
+    case 'GET':
+      try {
+        const user = await userService.getUser(req.query.id as string);
+        if (!user) {
+          res.status(404).json({ message: 'user not found' });
+        } else {
+          res.status(200).json(user);
+        }
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    case 'POST':
+      try {
+        const user = await userService.createUser(req.body);
+        res.status(201).json(user);
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    case 'PUT':
+      try {
+        const user = await userService.updateUser(
+          req.query.id as string,
+          req.body
+        );
+        if (!user) {
+          res.status(404).json({ message: 'user not found' });
+        } else {
+          res.status(200).json(user);
+        }
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    case 'DELETE':
+      try {
+        const user = await userService.deleteUser(req.query.id as string);
+        if (!user) {
+          res.status(404).json({ message: 'user not found' });
+        } else {
+          res.status(200).json({ message: 'user deleted successfully' });
+        }
+      } catch (err: any) {
+        res.status(500).json({ message: err.message });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
