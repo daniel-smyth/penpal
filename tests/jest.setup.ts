b9cfca577652d694119b8d65b9ba6bf3ca1f0861
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-01-27 20:01:24 +1100

    Remove MSW mock server for Jest

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,16 +1,2 @@
 import '@testing-library/jest-dom';
-// import 'whatwg-fetch';
-
-// import { server } from './server/server';
-
-// // Establish API mocking before all tests.
-// beforeAll(() => server.listen());
-
-// // Reset any request handlers that we may add during the tests,
-// // so they don't affect other tests.
-// afterEach(() => server.resetHandlers());
-
-// // Clean up after the tests are finished.
-// afterAll(() => server.close());
-
 require('jest-fetch-mock').enableMocks();
