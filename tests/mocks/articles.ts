9effc91ff09ae7bfdeb19e64ecb18dd33b94355b
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-01-29 23:26:38 +1100

    Create mock next/router and mock article list
    Created a mock for the Next.js Router to enable testing of components that use the router for navigation.
    
    Create a mock containing multiple articles to test the article list of the user profile.

--- tests/mocks/articles.ts
+++ tests/mocks/articles.ts
@@ -1,1 +1,61 @@
-export const mockArticleList = [];
+import { IArticle } from '@lib/database/models';
+
+const makeArticle = (_id: string, title: string) => ({
+  _id,
+  title,
+  text: {
+    current: {
+      input: '',
+      output: {
+        choices: [{ text: '' }]
+      }
+    },
+    history: [
+      {
+        input: 'Test Text Input 1',
+        output: {
+          choices: [
+            { text: 'Test Text Output 1' },
+            { text: 'Test Text Output 2' }
+          ]
+        }
+      },
+      {
+        input: 'Test Text Input 2',
+        output: {
+          choices: [
+            { text: 'Test Text Output 3' },
+            { text: 'Test Text Output 4' }
+          ]
+        }
+      }
+    ]
+  },
+  image: {
+    current: {
+      input: '',
+      output: {
+        data: { url: '' }
+      }
+    },
+    history: [
+      {
+        input: 'Test Image Input 1',
+        output: { data: { url: 'Test Image Output 1' } }
+      },
+      {
+        input: 'Test Image Input 2',
+        output: { data: { url: 'Test Image Output 2' } }
+      }
+    ]
+  }
+});
+
+const mockArticleList: IArticle[] = [
+  makeArticle('1', 'Test Article 1'),
+  makeArticle('2', 'Test Article 2'),
+  makeArticle('3', 'Test Article 3'),
+  makeArticle('4', 'Test Article 4')
+];
+
+export default mockArticleList;
--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,2 +1,4 @@
 export { default as mockArticle } from './article';
+export { default as mockArticleList } from './articles';
 export { mockImageQueries, mockTextQueries } from './queries';
+export { default as mockNextRouter } from './router';
