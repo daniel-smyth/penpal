d001aa35555d5585c4c8bd2bf014145f837e073f
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-01-30 20:53:44 +1100

    Change article/[id] to a client component

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,11 +1,10 @@
-import { fetcher } from '@lib/fetcher';
-import { Article, IArticle } from '@lib/database/models';
+'use client';
+
 import {
   ImageGenerator,
   ShareArticle,
   TextGenerator
 } from '@components/app/article';
-import { articleService } from '@lib/database/services';
 import { useArticle } from '@lib/hooks';
 
 export default function EditArticlePage({
@@ -13,7 +12,7 @@ export default function EditArticlePage({
 }: {
   params: { id: string };
 }) {
-  const { article, mutate } = useArticle(id);
+  const { article } = useArticle(id);
 
   if (!article) {
     return <div>Loading...</div>;
