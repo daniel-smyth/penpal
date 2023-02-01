e61c7bda238a86a9f56248cc5a29f5e3cb1f8f20
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-01 20:39:36 +1100

    Modify article/[id] to be a server component
    Modify article/[id]/page.tsx to be a server component rather than use SWR.

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,18 +1,40 @@
-'use client';
-
+import mongoose from 'mongoose';
+import { articleService } from '@lib/database/services';
 import {
   ImageGenerator,
   ShareArticle,
   TextGenerator
 } from '@components/app/article';
-import { useArticle } from '@lib/hooks';
 
-export default function EditArticlePage({
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+async function getArticle(id: string) {
+  if (mongoose.isValidObjectId(id)) {
+    try {
+      mongoose.connect(MONGODB_URI!);
+      const article = await articleService.get(id);
+      if (!article) {
+        throw new Error('article not found');
+      }
+      return article.toObject();
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+}
+
+export default async function EditArticlePage({
   params: { id }
 }: {
   params: { id: string };
 }) {
-  const { article } = useArticle(id);
+  const article = await getArticle(id);
 
   if (!article) {
     return <div>Loading...</div>;
@@ -24,9 +46,9 @@ export default function EditArticlePage({
         <br />
         Title: {article.title}
         <br />
-        <TextGenerator article={article} />
+        {/* <TextGenerator article={article} />
         <ImageGenerator article={article} />
-        <ShareArticle article={article} />
+        <ShareArticle article={article} /> */}
       </>
     </main>
   );
