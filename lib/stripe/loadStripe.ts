3e2a9708c8de2c570ded8689455ea7704c8250c7
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-02 19:07:48 +1100

    Modify loadStripe.ts (for client) to check for .env vars

--- lib/stripe/loadStripe.ts
+++ lib/stripe/loadStripe.ts
@@ -2,22 +2,22 @@ import { Stripe, loadStripe } from '@stripe/stripe-js';
 
 let STRIPE_PUBLISHABLE_KEY = '';
 
-if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-  throw new Error(
-    'NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined. Please add it to your .env.local file.'
-  );
-}
-
-if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST) {
-  throw new Error(
-    'NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST is not defined. Please add it to your .env.local file.'
-  );
+if (process.env.NODE_ENV !== 'production') {
+  if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST) {
+    throw new Error(
+      'NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST is not defined. Please add it to your .env.local file.'
+    );
+  }
+  STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST;
 }
 
 if (process.env.NODE_ENV === 'production') {
+  if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+    throw new Error(
+      'NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined. Please add it to your .env.local file.'
+    );
+  }
   STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-} else {
-  STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST;
 }
 
 let stripePromise: Promise<Stripe | null>;
