05c9e45b9c8ef64289b0044cb4b6434a5860a4c7
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-02 17:03:06 +1100

    Implement basic loading.tsx pages for 2 pages
    Implemented for app/account/loading.tsx and app/article/[id]/loading.tsx

---
+++ app/account/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <main>
+      Name: Loading...
+      <br />
+      Email: Loading...
+      <br />
+      Articles:
+      <div>
+        <br />
+        <p>Loading title...</p>
+      </div>
+      <button>Delete</button>
+    </main>
+  );
+}
---
+++ app/article/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default async function EditArticleLoadingPage() {
+  return <main>Loading</main>;
+}
