b5d55183ddac8818d554daa832af3df0452dffcf
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-02 23:29:59 +1100

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,9 +2,6 @@ import NextAuth from 'next-auth';
 import { IUser } from '@lib/database/models';
 
 declare module 'next-auth' {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `Provider` React Context
-   */
   interface Session {
     user: IUser;
   }
