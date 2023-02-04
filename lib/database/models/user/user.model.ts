2e5b02899a767e7242cba45dac5e49c3b87a00c4
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-04 15:06:03 +1100

    Update user.model.ts with Stripe subscription fields
    The 3 fields are used to store information about the customer's Stripe account.
    
    stripeId: A unique identifier generated by the Stripe API that represents a customer in the system.
    
    subscriptionId: A unique identifier generated by the Stripe API that represents a subscription in the Stripe system.
    
    subscriptionStatus: A string that represents the current status of the customer's subscription on Stripe. It can be one of the following values: active, past_due, or cancelled.
    
    Having these fields in the user model allows you to easily store and manage customer information and their subscription status within your own application database.

--- lib/database/models/user/user.model.ts
+++ lib/database/models/user/user.model.ts
@@ -3,13 +3,15 @@ import { ObjectId } from 'mongodb';
 import { IArticle } from '@lib/database/models';
 
 export interface IUser {
-  id: string;
-  stripeId?: string;
+  _id?: string; // MongoDB ID
   name?: string | null;
   email?: string | null;
-  image?: string | null;
   emailVerified?: boolean;
+  image?: string | null;
   articles: (ObjectId | IArticle)[];
+  stripeId?: string;
+  subscriptionId?: string;
+  subscriptionStatus?: 'active' | 'past_due' | 'canceled';
 }
 
 const UserSchema = new Schema<IUser>({
@@ -25,12 +27,9 @@ const UserSchema = new Schema<IUser>({
     minlength: 1,
     maxlength: 100
   },
-  emailVerified: {
-    type: String
-  },
+  emailVerified: Boolean,
   image: {
     type: String,
-    required: true,
     lowercase: true,
     trim: true,
     minlength: 1
@@ -40,7 +39,24 @@ const UserSchema = new Schema<IUser>({
       type: Schema.Types.ObjectId,
       ref: 'Article'
     }
-  ]
+  ],
+  stripeId: {
+    type: String,
+    index: true,
+    unique: true,
+    minlength: 1
+  },
+  subscriptionId: {
+    type: String,
+    unique: true,
+    trim: true,
+    minlength: 1
+  },
+  subscriptionStatus: {
+    type: String,
+    trim: true,
+    minlength: 1
+  }
 });
 
 const User = mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
