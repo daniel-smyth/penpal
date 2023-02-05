660f59eb585c21fcf7784e3a26135452865737bb
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-05 20:16:57 +1100

    Add basic cancel functionality to checkout form

--- components/app/admin/subscription/CheckoutForm/CheckoutForm.tsx
+++ components/app/admin/subscription/CheckoutForm/CheckoutForm.tsx
@@ -9,6 +9,7 @@ import {
 } from '@stripe/react-stripe-js';
 import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
+import { fetcher } from '@lib/fetcher';
 
 interface CheckoutFormProps {
   redirectUrl: string;
@@ -22,7 +23,8 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({ redirectUrl }) => {
   const pathName = usePathname();
 
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [paymentMessage, setPaymentMessage] = useState('');
+  const [deleteMessage, setDeleteMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -41,16 +43,18 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({ redirectUrl }) => {
     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
       switch (paymentIntent?.status) {
         case 'succeeded':
-          setMessage('Payment succeeded!');
+          setPaymentMessage('Payment succeeded!');
           break;
         case 'processing':
-          setMessage('Your payment is processing.');
+          setPaymentMessage('Your payment is processing.');
           break;
         case 'requires_payment_method':
-          setMessage('Your payment was not successful, please try again.');
+          setPaymentMessage(
+            'Your payment was not successful, please try again.'
+          );
           break;
         default:
-          setMessage('Something went wrong.');
+          setPaymentMessage('Something went wrong.');
           break;
       }
     });
@@ -77,12 +81,20 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({ redirectUrl }) => {
     // confirming the payment. Otherwise, your customer will be redirected to
     // your `return_url`.
     if (error.type === 'card_error' || error.type === 'validation_error') {
-      setMessage(error.message || 'An unexpected error occurred.');
+      setPaymentMessage(error.message || 'An unexpected error occurred.');
     } else {
-      setMessage('An unexpected error occurred.');
+      setPaymentMessage('An unexpected error occurred.');
     }
 
     setIsLoading(false);
+  };
+
+  const handleClick = async () => {
+    const { message } = await fetcher({
+      url: '/api/stripe/subscription',
+      method: 'DELETE'
+    });
+    setDeleteMessage(message);
   };
 
   const paymentElementOptions = {
@@ -90,21 +102,25 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({ redirectUrl }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      Address:
-      <input type="text" />
-      <br />
-      {/* aligned to right */}
-      Plan Details: {pathName?.split('/').pop()}
-      <br />
-      <LinkAuthenticationElement onChange={(e) => setEmail(e.value.email)} />
-      <PaymentElement options={paymentElementOptions as any} />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
-        <span>{isLoading ? <div>Loading</div> : 'Pay now'}</span>
-      </button>
-      {/* Show any error or success messages */}
-      {message && <div>{message}</div>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        Address:
+        <input type="text" />
+        <br />
+        {/* aligned to right */}
+        Plan Details: {pathName?.split('/').pop()}
+        <br />
+        <LinkAuthenticationElement onChange={(e) => setEmail(e.value.email)} />
+        <PaymentElement options={paymentElementOptions as any} />
+        <button disabled={isLoading || !stripe || !elements} id="submit">
+          <span>{isLoading ? <div>Loading</div> : 'Pay now'}</span>
+        </button>
+        {/* Show any error or success messages */}
+        {paymentMessage && <div>{paymentMessage}</div>}
+      </form>
+      <button onClick={handleClick}>Cancel</button>
+      {deleteMessage}
+    </>
   );
 };
 
