ed9b87ded22387d17d3685e094acd79a5005b849
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-05 20:20:12 +1100

    Webhooks: Handle updating and deleting subscriptions
    StripeWebhookService class.
    
    Handle updating and creating subscriptions via events and
    webhooks. Currently sends an email when subscriptions start or when they are cancelled. Only works locally.

--- lib/stripe/server/stripe.webhook.service.ts
+++ lib/stripe/server/stripe.webhook.service.ts
@@ -2,83 +2,87 @@ import { emailService } from '@lib/email';
 import { userService } from '@lib/database/services';
 
 class StripeWebhookService {
-  public async onInvoicePaymentFailed(eventObject: { [key: string]: any }) {
-    const { customer: customerStripeId } = eventObject;
-
-    const user = await userService.find({ stripeId: customerStripeId });
-
-    await emailService.sendEmail({
-      from: 'penpal',
-      to: user?.email as string,
-      subject: 'Payment failed',
-      html: 'Your payment has failed. Please update your payment method.'
-    });
-  }
-
-  public async onSubscriptionCreated(eventObject: { [key: string]: any }) {
-    const {
-      id: subscriptionId,
-      status: subscriptionStatus,
-      customer: customerStripeId
-    } = eventObject;
-
-    const user = await userService.find({ stripeId: customerStripeId });
-
-    if (subscriptionStatus === 'active') {
-      await emailService.sendEmail({
-        from: 'penpal',
-        to: user?.email as string,
-        subject: 'Welcome to our service!',
-        html: 'Thank you for subscribing to our service.'
-      });
-    }
-
-    await userService.update(user?._id as string, {
-      ...user,
-      subscriptionId,
-      subscriptionStatus
-    });
-  }
-
   public async onSubscriptionUpdated(eventObject: { [key: string]: any }) {
-    const {
-      id: subscriptionId,
-      status: subscriptionStatus,
-      customer: customerStripeId
-    } = eventObject;
+    const { status: subscriptionStatus, customer: customerStripeId } =
+      eventObject;
 
     const user = await userService.find({ stripeId: customerStripeId });
-
     if (user) {
-      await userService.update(user._id, {
+      await userService.update(user.id, {
         ...user,
-        subscriptionId,
         subscriptionStatus
       });
     }
+
+    switch (subscriptionStatus) {
+      case 'trialing':
+        // The subscription is currently in a trial period and it’s safe to
+        // provision your product for your customer. The subscription
+        // transitions automatically to active when the first payment is made.
+        break;
+      case 'active':
+        await emailService.sendEmail({
+          from: 'danielsmyth2011@gmail.com',
+          to: user?.email as string,
+          subject: 'Subscription active',
+          html: ''
+        });
+        // The subscription is in good standing and the most recent payment
+        // is successful. It’s safe to provision your product for your customer.
+        break;
+      case 'incomplete':
+        // 	A successful payment needs to be made within 23 hours to activate
+        // the subscription. Or the payment requires action, like customer
+        // authentication. Read more about payments that require action.
+        // Subscriptions can also be incomplete if there’s a pending payment.
+        // In that case, the invoice status would be open_payment_pending and
+        // the PaymentIntent status would be processing.
+        break;
+      case 'incomplete_expired':
+        // The initial payment on the subscription failed and no successful
+        // payment was made within 23 hours of creating the subscription. These
+        // subscriptions don’t bill customers. This status exists so you can track
+        // customers that failed to activate their subscriptions.
+        break;
+      case 'past_due':
+        break;
+      case 'canceled':
+        await emailService.sendEmail({
+          from: 'danielsmyth2011@gmail.com',
+          to: user?.email as string,
+          subject: 'Subscription cancelled',
+          html: ''
+        });
+        // The subscription has been canceled.
+        break;
+      case 'unpaid':
+        // The latest invoice hasn’t been paid but the subscription remains in
+        // place. The latest invoice remains open and invoices continue to be
+        // generated but payments aren’t attempted. You should revoke access to
+        // your product when the subscription is unpaid since payments were
+        // already attempted and retried when it was past_due. To move the
+        // subscription to active, pay the most recent invoice before its due date.
+        break;
+    }
   }
 
-  public async onSubscriptionCancelled(eventObject: { [key: string]: any }) {
-    const {
-      id: subscriptionId,
-      status: subscriptionStatus,
-      customer: customerStripeId
-    } = eventObject;
+  public async onSubscriptionDeleted(eventObject: { [key: string]: any }) {
+    const { customer: customerStripeId } = eventObject;
 
     const user = await userService.find({ stripeId: customerStripeId });
+    if (user) {
+      delete user.subscriptionId;
+      delete user.subscriptionStatus;
 
-    await emailService.sendEmail({
-      from: 'penpal',
-      to: user?.email as string,
-      subject: 'Sad to see you go!',
-      html: 'We are sad to see you go. Please let us know if there is anything we can do to improve our service.'
-    });
+      await userService.update(user.id, { ...user });
 
-    await userService.update(user?._id as string, {
-      ...user,
-      subscriptionId,
-      subscriptionStatus
-    });
+      await emailService.sendEmail({
+        from: 'danielsmyth2011@gmail.com',
+        to: user.email as string,
+        subject: 'Subscription cancelled',
+        html: ''
+      });
+    }
   }
 }
 
