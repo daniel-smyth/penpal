9606cd0b206c1e21a14f66f666cece43e4bef963
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-06 22:25:25 +1100

    Configure tailwind to use Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 import './globals.css';
+import { Inter } from '@next/font/google';
+import { Navbar } from '@components/common';
 import Providers from './providers';
 
+const inter = Inter({ subsets: ['latin'] });
+
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html className={inter.className}>
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
