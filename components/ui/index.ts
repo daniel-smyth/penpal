4f14b69f0c1e721a4e67fcde79a047968420b7b8
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-07 19:02:20 +1100

    Create LoadingButton.tsx template partial

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Modal } from "./Modal/Modal";
 export { default as Leaflet } from "./Leaflet/Leaflet";
 export { default as Popover } from "./Popover/Popover";
+export { default as LoadingButton } from "./LoadingButton/LoadingButton";
