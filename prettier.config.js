ca6896de25fb4b2f3a8741632db714b174c8f189
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-07 13:30:04 +1100

    Update prettier.config.js and .prettierignore

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  arrowParens: 'always',
+  bracketSpacing: true,
   semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
   // pnpm doesn't support plugin autoloading
   // https://github.com/tailwindlabs/prettier-plugin-tailwindcss#installation
-  plugins: [require('prettier-plugin-tailwindcss')]
+  plugins: [require("prettier-plugin-tailwindcss")],
 };
