f8e41a2dceda1a3072c02618a8fecbec6594aa3e
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-13 22:05:42 +1100

    Delete unused loading.tsx pages

--- app/articles/[id]/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default async function ArticlePageLoading() {
-  return <main>Loading</main>;
-}
--- app/articles/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default async function ArticlesPageLoading() {
-  return <main>Loading</main>;
-}
