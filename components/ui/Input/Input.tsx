5ba90092f1ffa5b39dbf78d640050d58374991b2
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-13 22:01:32 +1100

    Create Index.tsx

---
+++ components/ui/Input/Input.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { SendIcon } from "lucide-react";
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  width?: string;
+  Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+const Input: React.FC<InputProps> = (props) => {
+  const { width } = props;
+
+  return (
+    <div className={`relative ${width && width}`}>
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        {props.type === "email" && (
+          <SendIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+        )}
+      </div>
+      <input
+        {...props}
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 outline-none focus:border-emerald-600 focus:ring-emerald-600  dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-emerald-600 dark:focus:ring-emerald-600"
+      />
+    </div>
+  );
+};
+
+export default Input;
--- components/ui/client.ts
+++ components/ui/client.ts
@@ -1,5 +1,5 @@
-export { default as Alert, type AlertProps } from "./Alert/Alert";
+export { default as Alert } from "./Alert/Alert";
 export { default as Modal } from "./Modal/Modal";
 export { default as Leaflet } from "./Leaflet/Leaflet";
+export { default as Button } from "./Button/ClientButton";
 export { default as Popover } from "./Popover/Popover";
-export { default as LoadingButton } from "./LoadingButton/LoadingButton";
