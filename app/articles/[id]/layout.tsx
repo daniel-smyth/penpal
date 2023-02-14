eb7d40b1dad9a74da3c7190239b62f15668d0c9c
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-14 12:34:40 +1100

    Update layout.tsx

--- app/articles/[id]/layout.tsx
+++ app/articles/[id]/layout.tsx
@@ -7,7 +7,7 @@ export default function ArticlesLayout({
 }) {
   return (
     <>
-      <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100 dark:from-gray-600 dark:via-gray-800 dark:to-zinc-900" />
+      <div className="fixed h-screen w-full bg-gradient-to-br from-slate-100 via-slate-100 to-slate-200 dark:from-gray-600 dark:via-gray-800 dark:to-zinc-900" />
       <ArticleNavbar />
       <main className="fixed w-full flex-col pt-16 sm:pl-64">{children}</main>
     </>
