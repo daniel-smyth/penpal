a7d644599eae59af04f5ecda042642098d98af5b
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-15 16:08:56 +1100

    Create article/[id]/head.tsx

---
+++ app/article/[id]/head.tsx
@@ -0,0 +1,5 @@
+import { Meta } from "@components/app/home";
+
+export default function Head() {
+  return <Meta />;
+}
--- components/app/article/index.ts
+++ components/app/article/index.ts
@@ -1,4 +1,5 @@
 export { default as ImageGenerator } from "./ImageGenerator/ImageGenerator";
 export { default as TextGenerator } from "./TextGenerator/TextGenerator";
+export { default as Meta } from "./Meta/Meta";
 export { default as LoadingPage } from "./LoadingPage/LoadingPage";
 export { default as ShareArticle } from "./ShareArticle/ShareArticle";
