665a75bc51d247e0409724795b997a51db7ffe65
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-15 12:07:30 +1100

    Update LeafletButton.tsx

--- components/common/Leaflet/LeafletButton.tsx
+++ components/common/Leaflet/LeafletButton.tsx
@@ -38,37 +38,40 @@ const LeafletButton: React.FC<LeafletButtonProps> = ({
   const logoButton = (
     <button
       {...rest}
-      className="group flex w-full items-center rounded-lg p-3 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+      className="group flex w-full items-center rounded-lg px-6 py-4 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
     >
       {Icon && (
-        <Icon className="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" />
+        <>
+          {loading ? (
+            <span className="spinner-grow spinner-sm inline-block">
+              <svg
+                className="h-6 w-6 animate-spin text-black dark:text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </span>
+          ) : (
+            <Icon className="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" />
+          )}
+        </>
       )}
       <span className="ml-4 inline-flex flex-1 items-center whitespace-nowrap text-left">
         {children}
-        {loading && (
-          <span className="spinner-grow spinner-sm ml-4 inline-block">
-            <svg
-              className="mr- ml-2 h-4 w-4 animate-spin text-black dark:text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          </span>
-        )}
       </span>
       {dropdownItems.length > 0 &&
         (!dropdown ? (
@@ -84,10 +87,7 @@ const LeafletButton: React.FC<LeafletButtonProps> = ({
       {hasDropdown ? logoButton : <Link href={href}>{logoButton}</Link>}
       {hasDropdown &&
         dropdownItems.map((item) => (
-          <ul
-            key={item.text}
-            className={`${!dropdown && `hidden`} space-y-2 py-2`}
-          >
+          <ul key={item.text} className={`${!dropdown && `hidden`}`}>
             <li>
               <Link
                 href={item.href}
