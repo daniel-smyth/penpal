4f28b1fde5f222403ad906532c6839d356403e4d
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-15 22:55:08 +1100

    Update ArticleList.tsx - handle SWR loading state

--- components/app/admin/articles/ArticleList/ArticleList.tsx
+++ components/app/admin/articles/ArticleList/ArticleList.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import useSWR from "swr";
 import { IArticle } from "@lib/database/models";
 import ArticleListItem from "./ArticleListItem/ArticleListItem";
+import LoadingPage from "../LoadingPage/LoadingPage";
 
 const fetchGet = (url: string) => fetch(url).then((res) => res.json());
 
@@ -14,15 +15,23 @@ interface ArticleListProps {
 const ArticleList: React.FC<ArticleListProps> = ({
   articles: fallbackData,
 }) => {
-  const { data: articles } = useSWR<IArticle[]>(`/api/article`, fetchGet, {
+  const {
+    data: articles,
+    isLoading,
+    isValidating,
+  } = useSWR<IArticle[]>(`/api/article`, fetchGet, {
     fallbackData,
   });
 
   return (
     <ul className="divide-y divide-gray-300 dark:divide-gray-700">
-      {articles?.map((article) => (
-        <ArticleListItem article={article} key={article._id} />
-      ))}
+      {isLoading || isValidating ? (
+        <LoadingPage />
+      ) : (
+        articles?.map((article) => (
+          <ArticleListItem article={article} key={article._id} />
+        ))
+      )}
     </ul>
   );
 };
