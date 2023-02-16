a0a223c15db5c7aea1b2b11ed5e81c1fa4e26711
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2023-02-16 14:53:10 +1100

    Update DashboardLayout.tsx

--- components/layouts/dashboard/DashboardLayout.tsx
+++ components/layouts/dashboard/DashboardLayout.tsx
@@ -21,7 +21,7 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({
   return (
     <>
       <div className="fixed h-screen w-full bg-gradient-to-br from-slate-100 via-slate-100 to-slate-200 dark:from-gray-600 dark:via-gray-800 dark:to-zinc-900" />
-      <div className="fixed top-0 w-full bg-white/0 transition-all">
+      <div className="fixed top-0 z-40 w-full bg-white/0 transition-all">
         <Navbar menuItems={menuItems} />
         <Sidebar menuItems={menuItems} />
       </div>
